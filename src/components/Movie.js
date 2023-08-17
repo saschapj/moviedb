f3f544f10155102ed699e808d282aba745103309
jungsp <jung_sascha@gmx.net>
2023-08-17 12:38:47 +0200

    added animation to movies

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -14,7 +14,7 @@ useEffect(()=>{
     {  moviesFromApi.map((movie)=>{
       
       return (        
-        <li className={`px-2 transition-opacity ease-in-out ${!showMovies && "opacity-0"}`} key={movie.id}>
+        <li className={` transition ease-in-out delay-150 shadow-md hover:shadow-2xl hover:scale-110 duration-300 px-2 transition-opacity ${!showMovies && "opacity-0"}`} key={movie.id}>
           <Link to={`/movie/${movie.id}`}>
         <img src={`https://image.tmdb.org/t/p/w300${movie.backdrop_path}`}/>
         <div className='text-xl font-bold'>{movie.title}</div>
